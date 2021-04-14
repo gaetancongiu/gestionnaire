47d350124cd7566fa1db5d8a5208c52eb80accec
congiu.gaetan <gaetan.congiu@esqese.ucly.fr>
2021-04-14 12:09:49 +0200

    Marqué une tâche comme terminée

--- src/TaskList.js
+++ src/TaskList.js
@@ -1,5 +1,18 @@
 import React from "react";
+
 function TaskList(props) {
+  const FinishTask = task =>
+    fetch("https://605375c645e4b30017291be6.mockapi.io/tasks/" + task.id, {
+      method: "PUT",
+      mode: "cors",
+      headers: {
+        "Content-type": "application/json; charset=UTF-8"
+      },
+      body: JSON.stringify({ isComplete: Boolean(true) }) //On tranforme la tâche cliqué en tâche terminé
+    })
+      .then(response => response.json())
+      .then(() => props.refresh()); //On rappelle l'API pour que nos 2 listes soit mis à jour
+
   return (
     <div className="TaskList">
       <div className="CurrentTask">
@@ -11,7 +24,8 @@ function TaskList(props) {
               item //puis on les affiche sous forme de liste
             ) => (
               <li key={item.id}>
-                {item.createdAt} : {item.description}
+                {item.createdAt.substr(0, 10)} : {item.description}
+                <button onClick={() => FinishTask(item)}>X</button>
               </li>
             ))}
         </ul>
